c448b5d7e740791db40ecfe4f3bd952739da6e8a
TT1312 <132920286+TT1312@users.noreply.github.com>
2024-04-21 23:17:44 +0530

    Add files via upload

---
+++ Java /Daily Practice Question/Rectangle/FindRectangle.java
@@ -0,0 +1,15 @@
+package com.rectangle;
+
+public class FindRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Rectangle rec = new Rectangle();
+		rec.l=20;
+		rec.b=30;
+		rec.calculate();
+
+	}
+
+}
---
+++ Java /Daily Practice Question/Rectangle/Rectangle.java
@@ -0,0 +1,15 @@
+package com.rectangle;
+
+public class Rectangle {
+	
+	int l;
+	int b;
+	
+	
+	public void calculate() {
+		// TODO Auto-generated method stub
+		int answer  = l*b;
+		System.out.println("Output is : " + answer);
+	}
+
+}
